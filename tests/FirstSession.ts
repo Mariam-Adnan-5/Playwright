ddf4d801911f26a7e6db43b9852c1c92dc7b06ac
Mariam Thabet <mariamadnan879@gmail.com>
2026-08-23 20:55:19 +0300

    Task 1

---
+++ tests/FirstSession.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+    //+ senario
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).press('CapsLock');
+  await page.getByRole('textbox', { name: 'Username' }).fill('A');
+  await page.getByRole('textbox', { name: 'Username' }).press('CapsLock');
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  //- senario
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('mm');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('12');
+  await page.getByRole('button', { name: 'Login' }).click();
+});
